f8b6b5f1b064595717433ea158e7ac43b4ca2931
David-solly <David.solomon_@outlook.com>
2020-08-18 19:18:15 +0100

    Added 2 way indexing and free space usage

--- xmap.go
+++ xmap.go
@@ -1,29 +1,60 @@
 package xmap
 
+import "sync"
+
 //Xmap : Creates an empty xmap Structure
 func Xmap() XMap {
 	return XMap{
-		xmap:     make(map[string]**interface{}),
-		xIDKy:    make(map[int]string),
-		elements: 0,
+		xmap:      make(map[string]**interface{}),
+		xIDKy:     make(map[int]string),
+		xKyID:     make(map[string]int),
+		elements:  0,
+		freeSpace: 0,
 	}
 
 }
 
 //XMap : Fast iteration type map struct - at least 10x faster
 type XMap struct {
-	xmap     map[string]**interface{}
-	xslice   []*interface{}
-	xIDKy    map[int]string
-	elements int
+	xmap      map[string]**interface{}
+	xslice    []*interface{}
+	xempty    []int
+	xIDKy     map[int]string
+	xKyID     map[string]int
+	elements  int
+	freeSpace int
+	mutex     sync.Mutex
 }
 
-//Add : add to the structure
+//Add : add to the structure by appending to structure
 func (p *XMap) Add(k string, v interface{}) {
 	p.xslice = append(p.xslice, &v)
 	p.xmap[k] = &p.xslice[p.elements]
 	p.xIDKy[p.elements] = k
+	p.xKyID[k] = p.elements
 	p.elements = len(p.xmap)
+
+}
+
+//AddEco : add to the structure by filling vacant spaces
+func (p *XMap) AddEco(k string, v interface{}) int {
+	if p.xempty != nil {
+		i := p.xempty[0]
+		p.xslice[i] = &v
+		p.xKyID[k] = i
+		if len(p.xempty) > 1 {
+			p.xempty = p.xempty[1:]
+			p.freeSpace = len(p.xempty)
+		} else {
+			p.xempty = nil
+			p.freeSpace = 0
+		}
+		return i
+	}
+
+	// Add a new value the usual way
+	p.Add(k, v)
+	return p.elements
 
 }
 
@@ -39,16 +70,47 @@ func (p *XMap) Update(i int, v interface{}) (previous, newValue interface{}) {
 //Delete :remove an entry using a key to the entry
 func (p *XMap) Delete(key string) {
 	if _, ok := p.xmap[key]; ok {
+		p.mutex.Lock()
 		delete(p.xmap, key)
+		p.DeleteElementAt(p.xKyID[key])
 		p.elements = len(p.xmap)
+		p.mutex.Unlock()
 	}
-
 }
 
 //DeleteElementAt : Delete an entry using a value index
 //Only sets the underlying slice location to nil
 func (p *XMap) DeleteElementAt(idx int) {
 	p.xslice[idx] = nil
+	p.xempty = append(p.xempty, idx)
+	p.freeSpace = len(p.xempty)
+}
+
+// IndexFreeSpace : Index the free space freed by Delete function
+func (p *XMap) IndexFreeSpace() {
+	go p.indexFreeSpace()
+}
+
+// FreeSpace : Available free space to re-populate
+func (p *XMap) FreeSpace() int {
+	return p.freeSpace
+}
+
+func (p *XMap) indexFreeSpace() {
+	ind := []int{}
+	for id, element := range p.xslice {
+		if element == nil {
+			ind = append(ind, id)
+		}
+	}
+
+	if len(ind) > 0 {
+		p.mutex.Lock()
+		p.xempty = ind
+		p.freeSpace = len(ind)
+		p.mutex.Unlock()
+	}
+
 }
 
 //RebuildIndex : Use this periodically to garbage collect nil values from underlying structure. WARNING: calling this function resets all the indexes, so if you had 5 items and removed #2, running this will shift the remaining indexes left to fill the empty slot
@@ -61,10 +123,14 @@ func (p *XMap) RebuildIndex() {
 
 		}
 	}
+	p.mutex.Lock()
 	p.elements = newm.elements
 	p.xIDKy = newm.xIDKy
 	p.xmap = newm.xmap
 	p.xslice = newm.xslice
+	p.freeSpace = 0
+	p.xempty = nil
+	p.mutex.Unlock()
 
 }
 
@@ -105,11 +171,11 @@ func (p *XMap) GetByIndex(index int) interface{} {
 }
 
 // GetIndexOf : Return the index of the item if it exists in the xmap structure, check for the boolean first as the default is to return nil on a non hit
-func (p *XMap) GetIndexOf(value interface{}) (index interface{}, exists bool) {
+func (p *XMap) GetIndexOf(value interface{}) (index int, exists bool) {
 	for i, e := range p.xslice {
-		if e == value {
+		if *e == value {
 			return i, true
 		}
 	}
-	return nil, false
+	return -1, false
 }
